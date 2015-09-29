698ccec37d2fab8ca9093bb58671298f6bbf822d
hanweitao <hanweitao@hanweitaodeMac-mini.local>
2015-09-29 19:59:14 +0800

    Initial Commit

---
+++ hello World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello World
+//
+//  Created by hanweitao on 15/9/29.
+//  Copyright (c) 2015年 mob. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
