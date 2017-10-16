a6cf30ad694e461da97262905148db36354ed5a9
Timo King <750895042@qq.com>
2017-10-16 14:34:18 +0800

    balabala

--- hello World/MyPlayground.playground/Contents.swift
+++ hello World/MyPlayground.playground/Contents.swift
@@ -75,8 +75,14 @@ let logTitle = "1234567890"
 
 
 //测试
+print("闭区间运算符:")
+for index in 1...5 {
+    print("\(index) * 5 = \(index * 5)")
+}
 
-
-
+print("半开区间运算符:")
+for index in 1..<5 {
+    print("\(index) * 5 = \(index * 5)")
+}
 
 
